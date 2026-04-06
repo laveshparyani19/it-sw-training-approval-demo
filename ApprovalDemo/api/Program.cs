4093f1174cd6341cf40ad173a91b4d3880814b0b
laveshparyani19 <lavesh.paryani@protego.services>
2026-04-06 11:25:09 +0530

    Move Supabase password to environment variable for security

--- ApprovalDemo/api/Program.cs
+++ ApprovalDemo/api/Program.cs
@@ -2,6 +2,12 @@ using ApprovalDemo.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Read Supabase password from environment variable and build connection string
+var supabasePassword = Environment.GetEnvironmentVariable("SUPABASE_PASSWORD") 
+    ?? throw new InvalidOperationException("SUPABASE_PASSWORD environment variable not set");
+var connectionString = $"Host=db.qxevtcviybjzqueipukf.supabase.co;Port=5432;Username=postgres;Password={supabasePassword};Database=postgres;SSL Mode=Require;Trust Server Certificate=true";
+builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
+
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
